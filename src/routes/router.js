70fb5364c696697ccbb761efb82602d39d1ae589
riotDev17 <tomasalberto527@gmail.com>
2024-01-23 12:24:49 +0700

    feat: CRUD agama selesai

--- src/routes/router.js
+++ src/routes/router.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import { authMiddleware } from '../middleware/authMiddleware.js';
 import adminController from '../controller/adminController.js';
+import agamaController from '../controller/agamaController.js';
 
 const router = express.Router();
 router.use(authMiddleware);
@@ -9,5 +10,12 @@ router.use(authMiddleware);
 router.get('/api/admin', adminController.getAdminController);
 router.put('/api/admin/:adminId', adminController.updateAdminController);
 router.delete('/api/admin/logout', adminController.logoutAdminController);
+
+// AGAMA
+router.get('/api/agama', agamaController.getAgamaController);
+router.post('/api/agama', agamaController.createAgamaController);
+router.get('/api/agama/:agamaId', agamaController.getAgamaByIdController);
+router.put('/api/agama/:agamaId', agamaController.updateAgamaController);
+router.delete('/api/agama/:agamaId', agamaController.deleteAgamaController);
 
 export { router };
---
+++ src/service/agamaService.js
@@ -0,0 +1,120 @@
+import { validation } from '../validation/validation.js';
+import { prismaClient } from '../app/database.js';
+import { ResponseError } from '../error/responseError.js';
+import {
+  getAgamaValidation,
+  createAgamaValidation,
+  deleteAgamaValidation,
+  updateAgamaValidation,
+} from '../validation/agamaValidation.js';
+
+const getAgamaService = async () => {
+  return prismaClient.agama.findMany({
+    select: {
+      id_agama: true,
+      nama_agama: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+};
+
+const createAgamaService = async (request) => {
+  const agama = await validation(createAgamaValidation, request);
+  const agamaExist = await prismaClient.agama.count({
+    where: {
+      nama_agama: agama.nama_agama,
+    },
+  });
+
+  if (agamaExist === 1) {
+    throw new ResponseError(409, 'Nama Agama Sudah Ada');
+  }
+
+  return prismaClient.agama.create({
+    data: agama,
+    select: {
+      id_agama: true,
+      nama_agama: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+const getAgamaByIdService = async (agamaId) => {
+  agamaId = await validation(getAgamaValidation, agamaId);
+  const agama = await prismaClient.agama.findFirst({
+    where: {
+      id_agama: agamaId,
+    },
+    select: {
+      id_agama: true,
+      nama_agama: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+
+  if (!agama) {
+    throw new ResponseError(404, 'Agama Tidak Ditemukan');
+  }
+
+  return agama;
+};
+
+const updateAgamaService = async (request) => {
+  const agama = await validation(updateAgamaValidation, request);
+  const agamaExist = await prismaClient.agama.count({
+    where: {
+      id_agama: agama.id_agama,
+    },
+  });
+
+  if (agamaExist !== 1) {
+    throw new ResponseError(404, 'Agama Tidak Ditemukan');
+  }
+
+  return prismaClient.agama.update({
+    where: {
+      id_agama: agama.id_agama,
+    },
+    data: agama,
+    select: {
+      id_agama: true,
+      nama_agama: true,
+      createdAt: true,
+      updatedAt: true,
+    },
+  });
+};
+
+const deleteAgamaService = async (agamaId) => {
+  agamaId = await validation(deleteAgamaValidation, agamaId);
+  const agamaExist = await prismaClient.agama.count({
+    where: {
+      id_agama: agamaId,
+    },
+  });
+
+  if (agamaExist !== 1) {
+    throw new ResponseError(404, 'Agama Tidak Ditemukan');
+  }
+
+  return prismaClient.agama.delete({
+    where: {
+      id_agama: agamaId,
+    },
+  });
+};
+
+export default {
+  getAgamaService,
+  getAgamaByIdService,
+  createAgamaService,
+  updateAgamaService,
+  deleteAgamaService,
+};
