dd854d091d6bd5e686810ec0825504a9558f79dc
riotDev17 <tomasalberto527@gmail.com>
2024-01-23 12:29:33 +0700

    docs: Membuat dokumentasi untuk API agama

---
+++ src/docs/routesDocs/agamaRoutesDocs.js
@@ -0,0 +1,191 @@
+// ======= GET AGAMA =======
+/**
+ * @openapi
+ * /api/agama:
+ *  get:
+ *    tags:
+ *      - Agama
+ *    summary: Get Agama
+ *    description: Get Agama
+ *    responses:
+ *      200:
+ *        description: Get Agama Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Get Agama Success'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Get Agama Unauthorized'
+ * */
+
+// ====== ADD AGAMA ======
+/**
+ * @openapi
+ * /api/agama:
+ *  post:
+ *    tags:
+ *      - Agama
+ *    summary: Add Agama
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Add Agama'
+ *    responses:
+ *      200:
+ *        description: Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Agama Success'
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Agama Bad Request'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Agama Unauthorized'
+ *      409:
+ *        description: Conflict
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Add Agama Conflict'
+ * */
+
+// ====== GET AGAMA BY ID ======
+/**
+ * @openapi
+ * /api/agama/{agamaId}:
+ *  get:
+ *    tags:
+ *      - Agama
+ *    summary: Get Agama By ID
+ *    description: Get Agama By ID
+ *    parameters:
+ *      - name: agamaId
+ *        in: path
+ *        required: true
+ *        description: id agama
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Get Agama By ID Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Get Agama By ID Success'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Get Agama By ID Unauthorized'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Get Agama By ID Not Found'
+ *
+ *
+ * */
+
+// ====== UPDATE AGAMA ======
+/**
+ * @openapi
+ * /api/agama/{agamaId}:
+ *  put:
+ *    tags:
+ *      - Agama
+ *    summary: Update Agama
+ *    description: Update Agama
+ *    parameters:
+ *      - name: agamaId
+ *        in: path
+ *        required: true
+ *        description: id agama
+ *        schema:
+ *          type: string
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Update Agama'
+ *    responses:
+ *      200:
+ *        description: Update Agama Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Agama Success'
+ *      400:
+ *        description: Bad Request
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Agama Bad Request'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Agama Unauthorized'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Update Agama Not Found'
+ *
+ * */
+
+// ====== DELETE AGAMA ======
+/**
+ * @openapi
+ * /api/agama/{agamaId}:
+ *  delete:
+ *    tags:
+ *      - Agama
+ *    summary: Delete Agama
+ *    description: Delete Agama
+ *    parameters:
+ *      - name: agamaId
+ *        in: path
+ *        required: true
+ *        description: id agama
+ *        schema:
+ *          type: string
+ *    responses:
+ *      200:
+ *        description: Delete Agama Success
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Delete Agama Success'
+ *      401:
+ *        description: Unauthorized
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Delete Agama Unauthorized'
+ *      404:
+ *        description: Not Found
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Delete Agama Not Found'
+ *
+ * */
