7c81443953038d86d02a8df225f9fb560be6c2f9
riotDev17 <tomasalberto527@gmail.com>
2024-01-23 11:06:29 +0700

    chore: merubah pesan error middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -3,20 +3,26 @@ import jwt from 'jsonwebtoken';
 export const authMiddleware = async (req, res, next) => {
   const { authorization } = req.headers;
   if (!authorization) {
-    return res.status(401).json({
-      status: 'Unauthorized',
-      message: 'Token tidak tersedia',
-    }).end();
+    return res
+      .status(401)
+      .json({
+        status: 'Unauthorized',
+        message: `HTTP 401 : Token tidak tersedia`,
+      })
+      .end();
   }
 
   try {
     const token = authorization.split(' ')[1];
     req.admin = jwt.verify(token, process.env.SECRET_KEY);
   } catch (e) {
-    return res.status(401).json({
-      status: 'Unauthorized',
-      message: 'Token tidak valid',
-    }).end();
+    return res
+      .status(401)
+      .json({
+        status: 'Unauthorized',
+        message: `HTTP 401 : Token tidak valid`,
+      })
+      .end();
   }
 
   next();
