52b818685a80e74147f84e4eba1cd11e23f531f8
riotDev17 <tomasalberto527@gmail.com>
2024-01-25 23:02:38 +0700

    docs: membuat dokumentasi API laporan barang masuk

---
+++ src/docs/routesDocs/laporanBarangMasukRoutesDocs.js
@@ -0,0 +1,19 @@
+// GET
+/**
+ * @openapi
+ * /api/laporan-barang-masuk:
+ *   get:
+ *     tags:
+ *       - Laporan Barang Masuk
+ *     summary: Get Laporan Barang Masuk
+ *     description: Get Laporan Barang Masuk
+ *     responses:
+ *       200:
+ *         description: Get Laporan Barang Masuk Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Get Laporan Barang Masuk Success'
+ * 
+ * 
+ */
